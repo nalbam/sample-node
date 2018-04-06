f07eb8e94307ee76598ecdd6d9fbb70783c5145f
유정열 <me@nalbam.com>
2018-04-06 11:57:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ let http = require('http');
 let handle = function(req, res) {
   console.log('Received request for URL: ' + req.url);
   res.writeHead(200);
-  res.end('<p>Hello World!</p>');
+  let date = new Date();
+  res.end('<p>Hello World! - ' + date + '</p>');
 };
 
 let www = http.createServer(handle);

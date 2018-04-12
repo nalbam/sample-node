b3ba3521872d15f4fbbdc7662fb4995edf287895
nalbam <me@nalbam.com>
2018-04-12 16:39:05 +0900

    date

--- src/main/node/server.js
+++ src/main/node/server.js
@@ -1,11 +1,11 @@
 let http = require('http'),
     moment = require("moment-timezone");
 
-let handle = function(req, res) {
-  console.log('Received request for URL: ' + req.url);
-  res.writeHead(200);
-  let date = moment().tz("Asia/Seoul").format();
-  res.end('<p>Hello World! - ' + date + '</p>');
+let handle = function (req, res) {
+    console.log('Received request for URL: ' + req.url);
+    res.writeHead(200);
+    let date = moment().tz("Asia/Seoul").format();
+    res.end('<p>Hello World!</p>' + date);
 };
 
 let server = http.createServer(handle);

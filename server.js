4f979811079587539307a66d4cb611d5374b737f
nalbam <me@nalbam.com>
2018-07-03 19:23:54 +0900

    nodejs logo

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ let os = require('os'),
 
 let handle = function (req, res) {
     console.log('Received request for URL: ' + req.url);
-    res.writeHead(200);
+    let logo = 'https://nodejs.org/static/images/logo.svg';
+    let host = os.hostname();
     let date = moment().tz("Asia/Seoul").format();
-    res.end('<h1>Hello World!</h1><p>' + os.hostname() + '</p><p>' + date + '</p>');
+    res.writeHead(200);
+    res.end('<h1>Hello Nodejs!</h1><p><img src="' + logo + '"></p><p>' + host + '</p><p>' + date + '</p>');
 };
 
 let server = http.createServer(handle);

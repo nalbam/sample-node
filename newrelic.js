30c68c2ea688c6368c00eff6d89b4cf80a2c06e0
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-07-02 11:24:38 +0900

    license_key

--- newrelic.js
+++ newrelic.js
@@ -14,7 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: 'xxxxxxxxxxxxxxxxxxxx',
+  license_key: '',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing

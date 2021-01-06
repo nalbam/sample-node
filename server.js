1c04d106bf2d18fb7273c957d7bf25dd74dab7ad
nalbam <me@nalbam.com>
2021-01-06 10:45:15 +0900

    add cluster

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const DD_AGENT = process.env.DD_AGENT_ENABLED || false;
 
 // datadog tracer
 if (DD_AGENT) {
-    require('dd-trace').init({
-        // hostname: process.env.DD_AGENT_HOST,
-        // port: process.env.DD_AGENT_PORT,
-        // analytics: true,
-    })
+  require('dd-trace').init({
+    // hostname: process.env.DD_AGENT_HOST,
+    // port: process.env.DD_AGENT_PORT,
+    // analytics: true,
+  })
 }
 
 // // newrelic tracer
@@ -18,6 +18,7 @@ if (DD_AGENT) {
 
 // env
 const PORT = process.env.PORT || 3000;
+const CLUSTER = process.env.CLUSTER || 'local';
 const PROFILE = process.env.PROFILE || 'default';
 const VERSION = process.env.VERSION || 'v0.0.0';
 const MESSAGE = process.env.MESSAGE || PROFILE;
@@ -25,12 +26,12 @@ const FAULT_RATE = process.env.FAULT_RATE || 0;
 const REDIS_URL = process.env.REDIS_URL || `redis://sample-node-redis:6379`;
 
 const os = require('os'),
-    cors = require('cors'),
-    express = require('express'),
-    moment = require('moment-timezone'),
-    redis = require('redis'),
-    request = require('request'),
-    apiMetrics = require('prometheus-api-metrics');
+  cors = require('cors'),
+  express = require('express'),
+  moment = require('moment-timezone'),
+  redis = require('redis'),
+  request = require('request'),
+  apiMetrics = require('prometheus-api-metrics');
 
 // jaeger
 // const initTracer = require('jaeger-client').initTracer;
@@ -58,7 +59,7 @@ const os = require('os'),
 
 const Tracer = require('@risingstack/jaeger');
 const tracer = new Tracer({
-    serviceName: 'sample-node'
+  serviceName: 'sample-node'
 });
 
 // // zipkin
@@ -92,315 +93,316 @@ app.use(apiMetrics())
 
 // redis
 const retry_strategy = function (options) {
-    if (options.error && (options.error.code === 'ECONNREFUSED' || options.error.code === 'NR_CLOSED')) {
-        // Try reconnecting after 5 seconds
-        console.error('The server refused the connection. Retrying connection...');
-        return 5000;
-    }
-    if (options.total_retry_time > 1000 * 60 * 60) {
-        // End reconnecting after a specific timeout and flush all commands with an individual error
-        return new Error('Retry time exhausted');
-    }
-    if (options.attempt > 50) {
-        // End reconnecting with built in error
-        return undefined;
-    }
-    // reconnect after
-    return Math.min(options.attempt * 100, 5000);
+  if (options.error && (options.error.code === 'ECONNREFUSED' || options.error.code === 'NR_CLOSED')) {
+    // Try reconnecting after 5 seconds
+    console.error('The server refused the connection. Retrying connection...');
+    return 5000;
+  }
+  if (options.total_retry_time > 1000 * 60 * 60) {
+    // End reconnecting after a specific timeout and flush all commands with an individual error
+    return new Error('Retry time exhausted');
+  }
+  if (options.attempt > 50) {
+    // End reconnecting with built in error
+    return undefined;
+  }
+  // reconnect after
+  return Math.min(options.attempt * 100, 5000);
 };
 const client = redis.createClient(REDIS_URL, {
-    retry_strategy: retry_strategy
+  retry_strategy: retry_strategy
 });
 client.on('connect', () => {
-    console.log(`connected to redis: ${REDIS_URL}`);
+  console.log(`connected to redis: ${REDIS_URL}`);
 });
 client.on('error', err => {
-    console.error(`${err}`);
+  console.error(`${err}`);
 });
 
 app.get('/', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    let host = os.hostname();
-    let date = moment().tz('Asia/Seoul').format();
-    res.render('index.ejs', {
-        host: host,
-        date: date,
-        profile: PROFILE,
-        message: MESSAGE,
-        version: VERSION,
-    });
+  // console.log(`${req.method} ${req.path}`);
+  let host = os.hostname();
+  let date = moment().tz('Asia/Seoul').format();
+  res.render('index.ejs', {
+    host: host,
+    date: date,
+    cluster: CLUSTER,
+    profile: PROFILE,
+    message: MESSAGE,
+    version: VERSION,
+  });
 });
 
 app.get('/drop', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    res.render('drop.ejs', {
-        rate: 100,
-    });
+  // console.log(`${req.method} ${req.path}`);
+  res.render('drop.ejs', {
+    rate: 100,
+  });
 });
 
 app.get('/drop/:rate', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    res.render('drop.ejs', {
-        rate: req.params.rate,
-    });
+  // console.log(`${req.method} ${req.path}`);
+  res.render('drop.ejs', {
+    rate: req.params.rate,
+  });
 });
 
 app.get('/read', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    return res.status(200).json({
-        result: 'read',
-    });
+  // console.log(`${req.method} ${req.path}`);
+  return res.status(200).json({
+    result: 'read',
+  });
 });
 
 app.get('/live', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    return res.status(200).json({
-        result: 'live',
-    });
+  // console.log(`${req.method} ${req.path}`);
+  return res.status(200).json({
+    result: 'live',
+  });
 });
 
 app.get('/health', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    var version;
-    if (PROFILE === 'default') {
-        version = `v0.0.${parseInt(Math.random() * 2)}`;
-    } else {
-        version = VERSION;
-    }
+  // console.log(`${req.method} ${req.path}`);
+  var version;
+  if (PROFILE === 'default') {
+    version = `v0.0.${parseInt(Math.random() * 2)}`;
+  } else {
+    version = VERSION;
+  }
 
-    if (Math.random() * 100 >= FAULT_RATE) {
-        return res.status(200).json({
-            result: 'ok',
-            version: version,
-        });
-    } else {
-        return res.status(500).json({
-            result: 'error',
-            version: version,
-        });
-    }
+  if (Math.random() * 100 >= FAULT_RATE) {
+    return res.status(200).json({
+      result: 'ok',
+      version: version,
+    });
+  } else {
+    return res.status(500).json({
+      result: 'error',
+      version: version,
+    });
+  }
 });
 
 app.get('/spring', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    console.log(`${JSON.stringify(req.headers)}`);
+  // console.log(`${req.method} ${req.path}`);
+  console.log(`${JSON.stringify(req.headers)}`);
 
-    var remoteService;
-    if (PROFILE === 'default') {
-        remoteService = 'localhost:8080';
+  var remoteService;
+  if (PROFILE === 'default') {
+    remoteService = 'localhost:8080';
+  } else {
+    remoteService = 'sample-spring:8080';
+  }
+
+  request(`http://${remoteService}/health`, function (error, response, body) {
+    if (error) {
+      return res.status(500).json({
+        result: 'error',
+      });
     } else {
-        remoteService = 'sample-spring:8080';
+      return res.status(response.statusCode).json(JSON.parse(body));
     }
-
-    request(`http://${remoteService}/health`, function (error, response, body) {
-        if (error) {
-            return res.status(500).json({
-                result: 'error',
-            });
-        } else {
-            return res.status(response.statusCode).json(JSON.parse(body));
-        }
-    });
+  });
 });
 
 app.get('/tomcat', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    console.log(`${JSON.stringify(req.headers)}`);
+  // console.log(`${req.method} ${req.path}`);
+  console.log(`${JSON.stringify(req.headers)}`);
 
-    var remoteService;
-    if (PROFILE === 'default') {
-        remoteService = 'localhost:8080';
+  var remoteService;
+  if (PROFILE === 'default') {
+    remoteService = 'localhost:8080';
+  } else {
+    remoteService = 'sample-tomcat:8080';
+  }
+
+  request(`http://${remoteService}/health`, function (error, response, body) {
+    if (error) {
+      return res.status(500).json({
+        result: 'error'
+      });
     } else {
-        remoteService = 'sample-tomcat:8080';
+      return res.status(response.statusCode).json(JSON.parse(body));
     }
-
-    request(`http://${remoteService}/health`, function (error, response, body) {
-        if (error) {
-            return res.status(500).json({
-                result: 'error'
-            });
-        } else {
-            return res.status(response.statusCode).json(JSON.parse(body));
-        }
-    });
+  });
 });
 
 app.get('/loop/:count', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    console.log(`${JSON.stringify(req.headers)}`);
+  // console.log(`${req.method} ${req.path}`);
+  console.log(`${JSON.stringify(req.headers)}`);
 
-    var count = req.params.count;
+  var count = req.params.count;
 
-    if (count <= 0) {
-        return res.status(200).json({
-            result: 'ok',
-        });
-    }
-
-    count--;
-
-    var remoteService;
-    if (PROFILE === 'default') {
-        remoteService = 'localhost:3000';
-    } else {
-        remoteService = 'sample-node:3000';
-    }
-
-    // const zipRequest = zipkinRequest(request, {
-    //     tracer,
-    //     remoteService
-    // });
-
-    // zipRequest({
-    //     url: `http://${remoteService}/loop/${count}`,
-    //     method: 'GET',
-    // }, function (error, response, body) {
-    //     if (error) {
-    //         return res.status(500).json({
-    //             result: 'error'
-    //         });
-    //     } else {
-    //         return res.status(response.statusCode).json({
-    //             result: 'ok',
-    //             data: JSON.parse(body)
-    //         });
-    //     }
-    // });
-
-    request(`http://${remoteService}/loop/${count}`, function (error, response, body) {
-        // console.log('error:', error);
-        // console.log('statusCode:', response && response.statusCode);
-        // console.log('body:', body);
-
-        if (error) {
-            return res.status(500).json({
-                result: 'error',
-            });
-        } else {
-            return res.status(response.statusCode).json({
-                result: 'ok',
-                data: JSON.parse(body)
-            });
-        }
+  if (count <= 0) {
+    return res.status(200).json({
+      result: 'ok',
     });
+  }
+
+  count--;
+
+  var remoteService;
+  if (PROFILE === 'default') {
+    remoteService = 'localhost:3000';
+  } else {
+    remoteService = 'sample-node:3000';
+  }
+
+  // const zipRequest = zipkinRequest(request, {
+  //     tracer,
+  //     remoteService
+  // });
+
+  // zipRequest({
+  //     url: `http://${remoteService}/loop/${count}`,
+  //     method: 'GET',
+  // }, function (error, response, body) {
+  //     if (error) {
+  //         return res.status(500).json({
+  //             result: 'error'
+  //         });
+  //     } else {
+  //         return res.status(response.statusCode).json({
+  //             result: 'ok',
+  //             data: JSON.parse(body)
+  //         });
+  //     }
+  // });
+
+  request(`http://${remoteService}/loop/${count}`, function (error, response, body) {
+    // console.log('error:', error);
+    // console.log('statusCode:', response && response.statusCode);
+    // console.log('body:', body);
+
+    if (error) {
+      return res.status(500).json({
+        result: 'error',
+      });
+    } else {
+      return res.status(response.statusCode).json({
+        result: 'ok',
+        data: JSON.parse(body)
+      });
+    }
+  });
 });
 
 app.get('/stress', function (req, res) {
-    console.log(`${req.method} ${req.path}`);
-    let sum = 0;
-    for (let i = 0; i < 1000000; i++) {
-        sum += Math.sqrt(i);
-    }
-    return res.status(200).json({
-        result: 'ok',
-        sum: sum,
-    });
+  console.log(`${req.method} ${req.path}`);
+  let sum = 0;
+  for (let i = 0; i < 1000000; i++) {
+    sum += Math.sqrt(i);
+  }
+  return res.status(200).json({
+    result: 'ok',
+    sum: sum,
+  });
 });
 
 app.get('/success/:rate', function (req, res) {
-    console.log(`${req.method} ${req.path}`);
-    const rate = req.params.rate;
-    if (Math.random() * 100 <= rate) {
-        return res.status(200).json({
-            result: 'ok',
-            version: VERSION,
-        });
-    } else {
-        return res.status(500).json({
-            result: 'error',
-            version: VERSION,
-        });
-    }
+  console.log(`${req.method} ${req.path}`);
+  const rate = req.params.rate;
+  if (Math.random() * 100 <= rate) {
+    return res.status(200).json({
+      result: 'ok',
+      version: VERSION,
+    });
+  } else {
+    return res.status(500).json({
+      result: 'error',
+      version: VERSION,
+    });
+  }
 });
 
 app.get('/fault/:rate', function (req, res) {
-    console.log(`${req.method} ${req.path}`);
-    const rate = req.params.rate;
-    if (Math.random() * 100 >= rate) {
-        return res.status(200).json({
-            result: 'ok',
-            version: VERSION,
-        });
-    } else {
-        return res.status(500).json({
-            result: 'error',
-            version: VERSION,
-        });
-    }
+  console.log(`${req.method} ${req.path}`);
+  const rate = req.params.rate;
+  if (Math.random() * 100 >= rate) {
+    return res.status(200).json({
+      result: 'ok',
+      version: VERSION,
+    });
+  } else {
+    return res.status(500).json({
+      result: 'error',
+      version: VERSION,
+    });
+  }
 });
 
 app.get('/cache/:name', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    const name = req.params.name;
-    return client.get(`cache:${name}`, (err, result) => {
-        if (err) {
-            console.error(`${err}`);
-            return res.status(500).json({
-                status: 500,
-                message: err.message,
-            });
-        }
-        return res.status(200).json(result == null ? {} : JSON.parse(result));
-    });
+  // console.log(`${req.method} ${req.path}`);
+  const name = req.params.name;
+  return client.get(`cache:${name}`, (err, result) => {
+    if (err) {
+      console.error(`${err}`);
+      return res.status(500).json({
+        status: 500,
+        message: err.message,
+      });
+    }
+    return res.status(200).json(result == null ? {} : JSON.parse(result));
+  });
 });
 
 app.post('/cache/:name', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    const name = req.params.name;
-    const json = JSON.stringify(req.body);
-    //console.log(`req.body: ${json}`);
-    return client.set(`cache:${name}`, json, (err, result) => {
-        if (err) {
-            console.error(`${err}`);
-            return res.status(500).json({
-                status: 500,
-                message: err.message,
-            });
-        }
-        return res.status(200).json(result == null ? {} : result);
-    });
+  // console.log(`${req.method} ${req.path}`);
+  const name = req.params.name;
+  const json = JSON.stringify(req.body);
+  //console.log(`req.body: ${json}`);
+  return client.set(`cache:${name}`, json, (err, result) => {
+    if (err) {
+      console.error(`${err}`);
+      return res.status(500).json({
+        status: 500,
+        message: err.message,
+      });
+    }
+    return res.status(200).json(result == null ? {} : result);
+  });
 });
 
 app.get('/counter/:name', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    const name = req.params.name;
-    return client.get(`counter:${name}`, (err, result) => {
-        res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-        if (err) {
-            console.error(`${err}`);
-            return res.status(500).send(err.message);
-        }
-        return res.send(result == null ? '0' : result.toString());
-    });
+  // console.log(`${req.method} ${req.path}`);
+  const name = req.params.name;
+  return client.get(`counter:${name}`, (err, result) => {
+    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+    if (err) {
+      console.error(`${err}`);
+      return res.status(500).send(err.message);
+    }
+    return res.send(result == null ? '0' : result.toString());
+  });
 });
 
 app.post('/counter/:name', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    const name = req.params.name;
-    return client.incr(`counter:${name}`, (err, result) => {
-        res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-        if (err) {
-            console.error(`${err}`);
-            return res.status(500).send(err.message);
-        }
-        return res.send(result == null ? '0' : result.toString());
-    });
+  // console.log(`${req.method} ${req.path}`);
+  const name = req.params.name;
+  return client.incr(`counter:${name}`, (err, result) => {
+    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+    if (err) {
+      console.error(`${err}`);
+      return res.status(500).send(err.message);
+    }
+    return res.send(result == null ? '0' : result.toString());
+  });
 });
 
 app.delete('/counter/:name', function (req, res) {
-    // console.log(`${req.method} ${req.path}`);
-    const name = req.params.name;
-    return client.decr(`counter:${name}`, (err, result) => {
-        res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-        if (err) {
-            console.error(`${err}`);
-            return res.status(500).send(err.message);
-        }
-        return res.send(result == null ? '0' : result.toString());
-    });
+  // console.log(`${req.method} ${req.path}`);
+  const name = req.params.name;
+  return client.decr(`counter:${name}`, (err, result) => {
+    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+    if (err) {
+      console.error(`${err}`);
+      return res.status(500).send(err.message);
+    }
+    return res.send(result == null ? '0' : result.toString());
+  });
 });
 
 app.listen(PORT, function () {
-    console.log(`[${PROFILE}] Listening on port ${PORT}!`);
-    console.log(`connecting to redis: ${REDIS_URL}`);
+  console.log(`[${PROFILE}] Listening on port ${PORT}!`);
+  console.log(`connecting to redis: ${REDIS_URL}`);
 });
